d3dee1588836268ef0c4fd1e17b308873aeb7111
maryoma497 <maryoo14medo15@gmail.com>
2025-08-28 17:51:22 +0300

    update

---
+++ script.js
@@ -0,0 +1,25 @@
+document.getElementById("darkToggle").addEventListener("click", function () {
+    document.body.classList.toggle("dark-mode");
+});
+
+
+function myFunction() {
+    document.getElementById("myDropdown").classList.toggle("show");
+  }
+  
+  window.onclick = function(event) {
+    if (!event.target.matches('.dropbtn')) {
+      var dropdowns = document.getElementsByClassName("dropdown-content");
+      var i;
+      for (i = 0; i < dropdowns.length; i++) {
+        var openDropdown = dropdowns[i];
+        if (openDropdown.classList.contains('show')) {
+          openDropdown.classList.remove('show');
+        }
+      }
+    }
+  }
+
+  document.getElementById("darkToggle").addEventListener("click", function () {
+    document.body.classList.toggle("dark-mode");
+});
